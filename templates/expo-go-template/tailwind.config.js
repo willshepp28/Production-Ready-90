1fed4db32ef529767f8f168789c7de10c818114b
willshepp28 <willsheppard29@gmail.com>
2025-11-06 14:12:39 -0500

    setup tailwind.config.js so that tailwind is applied to files in components directory

--- templates/expo-go-template/tailwind.config.js
+++ templates/expo-go-template/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./App.{js,jsx,ts,tsx}",
     "./app/**/*.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
