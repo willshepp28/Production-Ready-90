513b6603c9750d7af81834788dc4cebbccb5e403
willshepp28 <willsheppard29@gmail.com>
2025-11-07 15:01:15 -0500

    now rendering differnt ui based on the state of whether user has added food or not in index.tsx

--- daily/day-03-favorite-foods-list/mobile/app/(tabs)/index.tsx
+++ daily/day-03-favorite-foods-list/mobile/app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Text, TouchableOpacity, View } from "react-native";
 import "../../global.css";
 
 const NoFoods = () => (
-  <View className="flex-1 items-center justify-center bg-background">
+  <View className="flex-1 items-center justify-center">
     <Text className="font-bold text-primary text-8xl">🍽️</Text>
     <Text className="font-bold text-3xl">No Foods Yet!</Text>
     <Text>Tap the + button below to add your favorite foods to the list</Text>
@@ -16,5 +16,14 @@ const NoFoods = () => (
 
 export default function HomeScreen() {
   const { favoriteFoods } = useFoodStore();
-  return <NoFoods />;
+  const foodCount = favoriteFoods.length;
+  return (
+    <View className="flex-1 items-center justify-center">
+      {foodCount ? (
+        <Text className="text-red">You have foods</Text>
+      ) : (
+        <NoFoods />
+      )}
+    </View>
+  );
 }
