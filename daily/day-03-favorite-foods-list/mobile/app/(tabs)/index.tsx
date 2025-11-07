7a14f666ea2cb914ceccd8e7d25f764897f85a56
willshepp28 <willsheppard29@gmail.com>
2025-11-07 06:51:07 -0500

    created a component for the state when user has selected no favorite foods

--- daily/day-03-favorite-foods-list/mobile/app/(tabs)/index.tsx
+++ daily/day-03-favorite-foods-list/mobile/app/(tabs)/index.tsx
@@ -1,16 +1,20 @@
+import useFoodStore from "@/store/useFood";
 import { Text, TouchableOpacity, View } from "react-native";
 
 import "../../global.css";
 
+const NoFoods = () => (
+  <View className="flex-1 items-center justify-center bg-background">
+    <Text className="font-bold text-primary text-8xl">🍽️</Text>
+    <Text className="font-bold text-3xl">No Foods Yet!</Text>
+    <Text>Tap the + button below to add your favorite foods to the list</Text>
+    <TouchableOpacity className="mt-4 rounded-xl bg-primary px-4 py-2">
+      <Text className="text-white font-semibold">Get Started</Text>
+    </TouchableOpacity>
+  </View>
+);
+
 export default function HomeScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-background">
-      <Text className="font-bold text-primary text-8xl">🍽️</Text>
-      <Text className="font-bold text-3xl">No Foods Yet!</Text>
-      <Text>Tap the + button below to add your favorite foods to the list</Text>
-      <TouchableOpacity className="mt-4 rounded-xl bg-primary px-4 py-2">
-        <Text className="text-white font-semibold">Get Started</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  const { favoriteFoods } = useFoodStore();
+  return <NoFoods />;
 }
