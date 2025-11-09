6ccf8c79123fdd2f0157f9187a4505ea1bdc34d8
willshepp28 <willsheppard29@gmail.com>
2025-11-08 20:33:20 -0500

    when users clicks button to add food model now appears

--- daily/day-03-favorite-foods-list/mobile/app/(tabs)/index.tsx
+++ daily/day-03-favorite-foods-list/mobile/app/(tabs)/index.tsx
@@ -1,33 +1,46 @@
 import useFoodStore from "@/store/useFood";
 import { useState } from "react";
-import { Alert, Modal, Pressable, Text, View } from "react-native";
+import { Modal, Pressable, Text, View } from "react-native";
 
 import "../../global.css";
 
-type addFoodModalProps = {
+type AddFoodModalProps = {
   shouldBeVisible: boolean;
-  setVisible: boolean;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const addFoodModal = ({ shouldBeVisible, setVisible }: addFoodModalProps) => (
-  <Modal visible={shouldBeVisible}>
-    <View>
-      <Text>Hey there</Text>
+type NoFoodsProps = {
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const AddFoodModal = ({ shouldBeVisible, setVisible }: AddFoodModalProps) => (
+  <Modal visible={shouldBeVisible} transparent animationType="slide">
+    <View className="flex-1 bg-black/50 justify-end">
+      <View className="bg-white rounded-t-3xl px-6 pt-20 pb-8">
+        <View className=" items-center justify-center">
+          <View className="flex-row">
+            <Text className="font-bold text-4xl">Add Favorite Food</Text>
+            <Pressable
+              className="bg-gray-500 p-6"
+              onPress={() => setVisible(false)}
+            >
+              <Text className="text-white">x</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
     </View>
-    <Pressable onPress={() => setVisible(false)}>
-      <Text>Close Modal</Text>
-    </Pressable>
   </Modal>
 );
 
-const NoFoods = () => (
+const NoFoods = ({ setVisible }: NoFoodsProps) => (
   <View className="flex-1 items-center justify-center">
     <Text className="font-bold text-primary text-8xl">🍽️</Text>
     <Text className="font-bold text-3xl">No Foods Yet!</Text>
     <Text>Tap the + button below to add your favorite foods to the list</Text>
     <Pressable
-      className="bg-blue-700 rounded-full p-12"
-      onPress={() => Alert.alert("Adding Food")}
+      className="bg-blue-700 rounded-full p-12 mt-5"
+      onPress={() => setVisible(true)}
     >
       <Text className="text-white font-bold text-2xl">+</Text>
     </Pressable>
@@ -43,7 +56,10 @@ export default function HomeScreen() {
       {foodCount ? (
         <Text className="text-red">You have foods</Text>
       ) : (
-        <NoFoods />
+        <NoFoods setVisible={setVisible} />
+      )}
+      {visible && (
+        <AddFoodModal shouldBeVisible={visible} setVisible={setVisible} />
       )}
     </View>
   );
--- daily/day-03-favorite-foods-list/mobile/babel.config.js
+++ daily/day-03-favorite-foods-list/mobile/babel.config.js
@@ -1,9 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          alias: { "@": "./" },
+          extensions: [".tsx", ".ts", ".js", ".jsx", ".json"],
+        },
+      ],
+      "react-native-reanimated/plugin",
     ],
   };
 };
